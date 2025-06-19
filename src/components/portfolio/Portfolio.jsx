34078a021a61aa475d24d80c7b82e7963acc20ea
Mantas <109523731+mpociuipa@users.noreply.github.com>
2025-06-19 18:27:39 +0300

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -71,7 +71,7 @@ const Portfolio = () => {
               <h3>{title}</h3>
             <div className="portfolio__item-cta">
               <a href={github} className='btn' target='_blank'>Github</a>
-              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>To pay</a>
             </div>
             </article>
             )
