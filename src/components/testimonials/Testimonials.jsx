6a25f21ce01dd1fe018e6625c29cb137a5479055
Mantas <109523731+mpociuipa@users.noreply.github.com>
2025-07-04 18:37:16 +0300

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -54,7 +54,7 @@ const Testimonials = () => {
           return (
             <SwiperSlide key={index} className="testimonial">
             <div className="client__avatar">
-              <img src={avatar} alt="avatar"/>
+              <img src={avatar} alt="3D game"/>
             </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
