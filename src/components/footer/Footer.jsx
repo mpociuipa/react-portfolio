40d45cbd9a18d2e7747f83f73c780e70b5bfd5a3
Mantas <109523731+mpociuipa@users.noreply.github.com>
2026-02-02 17:21:30 +0200

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,28 +7,29 @@ import {IoLogoTwitter} from 'react-icons/io';
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>MANTAS</a>
+  <a href="#header" className="footer__logo">MANTAS</a>
 
-      <ul className='permalinks'>
-        <li><a href="#">home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+  <ul className="permalinks">
+    <li><a href="#header">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#experience">Experience</a></li>
+    <li><a href="#services">Services</a></li>
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><a href="#testimonials">Testimonials</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
 
-      <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebookF/></a>
-        <a href="https://instagram.com"><FiInstagram/></a>
-        <a href="https://twitter.com"><IoLogoTwitter/></a>
-      </div>
+  <div className="footer__socials">
+    <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+    <a href="https://instagram.com" target="_blank" rel="noreferrer"><FiInstagram /></a>
+    <a href="https://twitter.com" target="_blank" rel="noreferrer"><IoLogoTwitter /></a>
+  </div>
 
-      <div className="footer__copyright">
-        <small>&copy; EDAGOR Tutorials. All rights reserved.</small>
-      </div>
-    </footer>
+  <div className="footer__copyright">
+    <small>&copy; EDAGOR Tutorials. All rights reserved.</small>
+  </div>
+</footer>
+
   )
 }
 
