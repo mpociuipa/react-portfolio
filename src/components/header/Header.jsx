c0f6abae6d4153f867fa4be0f200bd7fc3e2b5a7
Mantas <109523731+mpociuipa@users.noreply.github.com>
2026-02-02 17:36:29 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,22 +6,24 @@ import HeaderSocial from './HeaderSocials';
 
 const Header = () => {
   return (
-    <header>
+    <header id="header">
       <div className="container header__container">
-          <h5>Hello I'm</h5>
-          <h1>Mantas Počiuipa</h1>
-          <h5 className="text-light">Fullstack Developer</h5>
-          <CTA />
-          <HeaderSocial />
+        <h5>Hello I'm</h5>
+        <h1>Mantas Počiuipa</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
 
-          <div className="me">
-            <img src={ME} alt="3D Games" />
-          </div>
+        <CTA />
+        <HeaderSocial />
 
-          <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <div className="me">
+          <img src={ME} alt="3D Games" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
+
