d7aa66a1a39f52b23ddc517d62639c15c59893e2
Mantas <109523731+mpociuipa@users.noreply.github.com>
2026-02-02 14:40:20 +0200

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,6 +9,7 @@ import IMG6 from '../../assets/Namas.jpg';
 import IMG7 from '../../assets/nama.jpg';
 import IMG8 from '../../assets/Javascript.png';
 import IMG9 from '../../assets/linux.png';
+import IMG10 from '../../assets/lektuvas.png';
 // DO NOT USE THE IMAGES IN PRODUCTION
 
 const data = [
@@ -74,6 +75,13 @@ const data = [
     title: 'Linux mokymai ant windows nemokamai (LT)',
     github: 'https://github.com',
     demo: 'https://manthispoc.gumroad.com/l/zkduwe'
+  },
+  {
+    id: 9,
+    image: IMG10,
+    title: 'Crimson Sky Firestorm Arena on android',
+    github: 'https://github.com',
+    demo: 'https://manthispoc.gumroad.com/l/dpfeuy'
   },
 ]
 
