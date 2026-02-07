13a6e27a623e3983eb931f1e0df26d14eca7d1c7
Mantas <109523731+mpociuipa@users.noreply.github.com>
2026-02-07 11:33:02 +0200

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,25 +5,43 @@ import ME from '../../assets/me.png';
 import HeaderSocial from './HeaderSocials';
 
 const Header = () => {
+
+  const skills = [
+    "Multi-stack Full Stack Developer",
+    "SQL + NoSQL Experience",
+    "Linux + Git (Engineering Base)",
+    "Web + Desktop Builder",
+    "Cloud Deployment Capable",
+    "Security-Inclined"
+  ];
+
   return (
     <header id="header">
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Mantas Počiuipa</h1>
-        <h5 className="text-light">Multi-stack full stack,</br> SQL + NoSQL experience,</br> Linux + Git user (engineering base),</br> Web + Desktop builder,</br> Cloud deploy capable,</br> Security-inclined</h5>
+
+        <h5 className="text-light">
+          {skills.map((skill, index) => (
+            <span key={index} className="header__skill-line">
+              {skill}
+            </span>
+          ))}
+        </h5>
 
         <CTA />
         <HeaderSocial />
 
         <div className="me">
-          <img src={ME} alt="3D Games" />
+          <img src={ME} alt="Mantas Počiuipa" />
         </div>
 
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
 };
 
 export default Header;
-
